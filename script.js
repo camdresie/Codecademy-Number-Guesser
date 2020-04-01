8606b5aaf30bbc863df734239302fd2c8a582a3a
Cam <cameron.riemensnider@gmail.com>
2020-04-01 16:15:39 -0700

    Additional JS code

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
+const generateTarget = () => {
+  const targetNumber = Math.floor(Math.random() * 9);
+  return targetNumber;
+};
+const compareGuesses = () => {
+   Math.abs(targetNumber - humanGuess)
+    if (Math.abs((targetNumber - humanGuess)) > (Math.abs(targetNumber - computerGuess))) {
+      return false;
+    } else if (humanGuess === computerGuess) {
+        return true;
+    } else {
+        return false;
+    }
+};
